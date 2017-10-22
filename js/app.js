6c27126f0abf175979b0d2a073a0902ca8dffa4a
Andrey Ivanov <andrey-tm203@mail.ru>
2017-10-22 15:54:17 +0300

    some js correctings

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function () {
     const mainNav = document.querySelector(".main-nav");
     const mainNavList = mainNav.querySelector(".main-nav__list");
 
     mainNavList.classList.remove("no-js");
-    mainNav.querySelector(".main-nav__toggle").addEventListener("click", () => {
+    mainNav.querySelector(".main-nav__toggle").addEventListener("click", function () {
         mainNav.classList.toggle("main-nav--opened");
     });
 });
