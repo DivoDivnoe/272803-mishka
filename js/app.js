37b7e1283048f6346d9d3e7dd5c1ed2d199ae130
Andrey Ivanov <andrey-tm203@mail.ru>
2017-10-27 11:42:04 +0300

    make correctings

--- js/app.js
+++ js/app.js
@@ -2,9 +2,8 @@
 
 document.addEventListener("DOMContentLoaded", function () {
     const mainNav = document.querySelector(".main-nav");
-    const mainNavList = mainNav.querySelector(".main-nav__list");
 
-    mainNavList.classList.remove("no-js");
+    mainNav.classList.remove("no-js");
     mainNav.querySelector(".main-nav__toggle").addEventListener("click", function () {
         mainNav.classList.toggle("main-nav--opened");
     });
